1c7beec5d6e9ae9a30e3b6a7fc6f2f499ddaad9a
Githuba Jeremy <githuba9520@gmail.com>
2024-03-21 13:13:21 +0300

    nesting cs using scsss

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,52 @@
+import { FaLaptopCode } from "react-icons/fa";
+function NavBar() {
+  const navlinks = [
+    "Home",
+    "Services",
+    "Testimonials",
+    "Faq",
+    "Portfolio",
+    "Contact",
+  ];
+
+  return (
+    <nav className="navbar navbar-expand-lg menu">
+      <div className="container brand-logo">
+        <FaLaptopCode />
+        <a className="navbar-brand" href="#">
+          Githubarj
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {navlinks.map((item, index) => (
+              <li key={index} className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button type="button" className="rounded-pill">
+            +254 798320724
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
